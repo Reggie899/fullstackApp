24fd9062a52b8dd4f6edd7276b2045fafb0802a4
Reggie899 <regina.schmidt1212@gmail.com>
2022-07-11 15:32:57 +0200

    update

---
+++ server/controllers/testing.js
@@ -0,0 +1,30 @@
+
+// import Test from '../models/Test.js';
+
+// export const getTest = (req, res) => {
+//     res.status(200).json({ message: 'my server is responding !!!' });
+// }
+
+// export const postTest = async (req, res) => {
+//     const { name } = req.body;
+//     const newTest = new Test({ name });
+//     try {
+//         await newTest.save();
+//         res.status(201).json(newTest);
+//     } catch (error) {
+//         res.status(409).json({ message: error.message });
+//     }
+
+// }
+
+export const getTest = (req, res) => {
+res.json("hello there!")
+}
+
+export const postTest = (req, res) => {
+const data = req.body
+res.json(
+    data
+)
+    }
+    
---
+++ server/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import testRouter from './routes/testRouter.js';
+
+
+
+const server = express();
+
+
+dotenv.config();
+
+
+server.use(cors()); 
+server.use(express.json()); 
+
+server.use('/test', testRouter);
+
+
+// connectDB();
+// mongoose.connection.on("open", () => {
+//         console.log("connected to db")
+// });
+// mongoose.connection.on("error", (error) => {
+// console.log("Connection to MongoDB has faild ", error.message);
+    
+// });
+
+const PORT = process.env.PORT || 8077;
+
+server.listen(PORT, () => console.log(`Server is listening to port ${PORT} and running`));
