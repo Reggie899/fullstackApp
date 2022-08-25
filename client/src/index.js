49e126d1ec93be15a572beed48cbe6977ae39740
Reggie899 <regina.schmidt1212@gmail.com>
2022-08-25 15:34:24 +0200

    strict mode gone

--- client/src/index.js
+++ client/src/index.js
@@ -7,11 +7,12 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
       <App />
-    </BrowserRouter>{" "}
-  </React.StrictMode>
+    </BrowserRouter>
+    // {" "}
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
